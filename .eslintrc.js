521550edce44588264b7409547223ef1bfdcaf8d
Pete Nelson <peter.nelson@act-on.com>
2020-02-12 20:39:43 -0800

    Fixed files with Prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,6 @@
 module.exports = {
-  plugins: ["prettier"],
   parser: "@typescript-eslint/parser", // Specifies the ESLint parser
   extends: [
-    "prettier",
     "plugin:react/recommended", // Uses the recommended rules from @eslint-plugin-react
     "plugin:@typescript-eslint/recommended", // Uses the recommended rules from the @typescript-eslint/eslint-plugin
     "prettier/@typescript-eslint", // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
