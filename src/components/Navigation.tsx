2dbbe9c453a2309639ba4e1936304dc5f6dcdb2c
Pete Nelson <peter.nelson@act-on.com>
2020-02-12 20:41:50 -0800

    Fixed lint script

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -2,9 +2,9 @@ import * as React from 'react'
 import { Link } from 'react-router-dom'
 
 export default function Navigation() {
-    return (
-        <div>
-            <Link to="/">Home</Link>
-        </div>
-    )
+  return (
+    <div>
+      <Link to="/">Home</Link>
+    </div>
+  )
 }
